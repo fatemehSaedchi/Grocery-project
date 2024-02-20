284967808966004d66c15a8ccc5773fd300091d4
Fatemeh <fatemeh.saedchi@gmail.com>
2024-02-20 14:33:38 +0330

    add styles and assets files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,91 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontSize:{
+        "17": "17px",
+        "18" :"18px",
+        "45": "45px",
+        "32": "32px",
+      },
+
+      colors:{
+        primary:{
+          "50": "#F2FCE4",
+          "100": "#DEF9EC",
+          "200":"#BCE3C9",
+          "300": "#3BB77E",
+          "400" : "#1C4037",
+        },
+
+        secondary:{
+          "100": "rgba(253,192,64,0.42)",
+          "200": "#FDC040",
+        },
+
+        Iteal:{
+          "50": "#D8F1E5",
+          "100": "rgba(80,123,166,0.1)",
+          "200": "#253D4E",
+        },
+
+        danger:{
+          "100": "#ef4f4f",
+        },
+
+        dark:{
+          "100": "#1b1b1b",
+        },
+
+        Igray:{
+          "50": "#F2F3F4",
+          "100": "#E5E5E5",
+          "200": "#B6B6B6",
+          "300": "#7E7E7E",
+        },
+
+
+        cream:{
+          "50" : "#E7EAF3",
+          "100": "#FFF3FF",
+          "200": "#F0E8D5",
+          "300": "#F3E8E8",
+        },
+
+        Ilavender:{
+          "50": "#f6abf6",
+        },
+      },
+
+      boxShadow:{
+        "shadow-1": "20px 20px 40px 0 rgba(24,24,24,0.07)",
+        "shadow-2" : "5px 5px 15px 0 rgba(24,24,24,0.07)",
+        "shadow-3" : "0 5px 30px 0 rgba(24,24,24,0.07)",
+      },
+
+      flexBasis:{
+        "31": "31%"
+      },
+
+      screens:{
+        "xs": "400px",
+      },
+
+      borderRadius:{
+        "10": "10px",
+      },
+
+      backgroundImage:{
+        "delivery-bg" : "url('../img/delivery section/3389452 1.png')",
+        "hero-bg": "url('../img/hero/heroBg1.png')"
+      },
+
+      fontFamily:{
+        "quicksand": ["Quicksand","sans"],
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
