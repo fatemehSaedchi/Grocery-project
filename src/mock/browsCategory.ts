7bc7eb84223e6a43f13ea859242fc1e6c89450c4
Fatemeh <fatemeh.saedchi@gmail.com>
2024-02-22 19:07:20 +0330

    implement browsCategory mock

---
+++ src/mock/browsCategory.ts
@@ -0,0 +1,64 @@
+export const  browsCategoryMock =[
+    {
+        icon: "icon-groceries-1",
+        iconPath: 7,
+        title: "Milk and Dairies",
+        link: "#",
+    },
+    {
+        icon: "icon-drinks-1",
+        iconPath: 5,
+        title: "Wines & Drinks",
+        link: "#",
+    },
+    {
+        icon: "icon-dress-1",
+        iconPath: 4,
+        title: "Clothing & Beauty",
+        link: "#",
+    },
+    {
+        icon: "icon-crab-1",
+        iconPath: 9,
+        title: "Fresh Sea Foods",
+        link: "#",
+    },
+    {
+        icon: "icon-pet-food-1",
+        iconPath: 8,
+        title: "Pet Foods & Toy",
+        link: "#",
+    },
+    {
+        icon: "icon-sweet-1",
+        iconPath: 7,
+        title: "Fast Food",
+        link: "#",
+    },
+    {
+        icon: "icon-dairy-products-1",
+        iconPath: 5,
+        title: "Backing material",
+        link: "#",
+    },
+    {
+        icon: "icon-grocery-1",
+        iconPath: 7,
+        title: "Vegetables",
+        link: "#",
+    },
+    {
+        icon: "icon-fruit-1",
+        iconPath: 3,
+        title: "Fresh Fruit",
+        link: "#",
+    },
+    {
+        icon: "icon-milk-products-1",
+        iconPath: 5,
+        title: "Bread and Juice",
+        link: "#",
+    },
+
+
+]
