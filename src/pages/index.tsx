8a4f4211266b7cc1554e7b5cc54684c93c3ddd8a
Fatemeh <fatemeh.saedchi@gmail.com>
2024-02-27 15:09:16 +0330

    implement featured category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import {Section} from "@/components/layouts";
-import {Banner} from "@/components";
+import {Banner, FeaturedCategory} from "@/components";
 
 
 export default function Home() {
@@ -11,6 +11,12 @@ export default function Home() {
                subtitle={"Sign up for the daily newsletter"}
                bgImage={"/assets/images/banner_bg.png"}
                image={"/assets/images/fresh-apples.png"}/>
+       </Section>
+       <Section>
+           <div className="hidden sm:flex mb-[50px]">
+               <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+           </div>
+           <FeaturedCategory/>
        </Section>
    </>
   );
