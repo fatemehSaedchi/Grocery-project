f02704f0e49425f3504cee3c620d1f0e5492baba
Fatemeh <fatemeh.saedchi@gmail.com>
2024-04-02 15:35:48 +0430

    edit ImageView structure

--- src/components/common/image-view/ImageView.tsx
+++ src/components/common/image-view/ImageView.tsx
@@ -3,16 +3,20 @@ import Image from 'next/image'
 
 
 interface Props {
-    src: string;
+    src?: string | null ;
     alt:string;
     width: number;
     height: number;
     classname?: string
 }
-export function ImageView({src,alt,width,height,classname=''}: Props) {
-    return (
-        <Image src={src} alt={alt} width={width} height={height} className={classname}/>
-    );
+export function ImageView({src='',alt,width,height,classname=''}: Props) {
+
+    const imageSrc = src ? (src.startsWith('/uploads') ? 'https://nest.navaxcollege.com' + src : src) : ''
+
+    return(
+        <Image src={imageSrc} alt={alt} width={width} height={height} className={classname}/>
+    )
+
 }
 
 
