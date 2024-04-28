24eeaad33df5e5c4a614165c2488c9cd4e97a87a
Fatemeh <fatemeh.saedchi@gmail.com>
2024-04-28 15:47:54 +0430

    implement observer in bottom slider

--- src/components/pages/home-page/tops-and-trending-slider/TopsAndTrendingSlider.tsx
+++ src/components/pages/home-page/tops-and-trending-slider/TopsAndTrendingSlider.tsx
@@ -3,6 +3,7 @@ import {Autoplay} from "swiper/modules";
 import {ProductVerticalList} from "@/components";
 import {useQuery} from "@tanstack/react-query";
 import {getAllProductsApiCall} from "@/api/Product";
+import { InView } from 'react-intersection-observer'
 
 
 export function TopsAndTrendingSlider() {
@@ -14,9 +15,10 @@ export function TopsAndTrendingSlider() {
                 page: 1,
                 pageSize: 3
             }
-        })})
-
-    console.log("topSellingData", topSellingData)
+        }),
+        enabled: false
+    })
+    // console.log("topSellingData", topSellingData)
 
     const {data: TrendingProductsData} = useQuery({queryKey: [getAllProductsApiCall.name, 'TrendingProductsData'], queryFn:()=> getAllProductsApiCall({
             populate: ["thumbnail"],
@@ -25,8 +27,10 @@ export function TopsAndTrendingSlider() {
                 page: 1,
                 pageSize: 3
             }
-        })})
-    console.log("TrendingProductsData", TrendingProductsData)
+        }),
+        enabled: false
+    })
+    // console.log("TrendingProductsData", TrendingProductsData)
 
     const {data: RecentlyAddedData} = useQuery({queryKey: [getAllProductsApiCall.name, 'RecentlyAddedData'], queryFn:()=> getAllProductsApiCall({
             populate: ["thumbnail"],
@@ -35,18 +39,22 @@ export function TopsAndTrendingSlider() {
                 page: 1,
                 pageSize: 3
             }
-        })})
-    console.log("RecentlyAddedData", RecentlyAddedData)
+        }),
+        enabled: false
+    })
+    // console.log("RecentlyAddedData", RecentlyAddedData)
 
-    const {data: topRatedData} = useQuery({queryKey: [getAllProductsApiCall.name, 'topRatedData'], queryFn:()=> getAllProductsApiCall({
+    const {data: topRatedData,refetch} = useQuery({queryKey: [getAllProductsApiCall.name, 'topRatedData'], queryFn:()=> getAllProductsApiCall({
             populate: ["thumbnail"],
             sort: ["rate:desc"],
             pagination: {
                 page: 1,
                 pageSize: 3
             }
-        })})
-    console.log("topRatedData", topRatedData)
+        }),
+        enabled: false
+    })
+    // console.log("topRatedData", topRatedData)
 
     return (
         <Swiper
@@ -70,19 +78,29 @@ export function TopsAndTrendingSlider() {
             }}
         >
             <SwiperSlide>
+                <InView as="div" onChange={(inView, entry) => inView && refetch()}>
                 {topSellingData && <ProductVerticalList title={'Top Selling'} data={topSellingData}/>}
+                </InView>
             </SwiperSlide>
 
             <SwiperSlide>
+                <InView as="div" onChange={(inView, entry) => inView && refetch()}>
                 {TrendingProductsData && <ProductVerticalList title={'Trending Products'} data={TrendingProductsData}/>}
+                </InView>
             </SwiperSlide>
 
+
             <SwiperSlide>
+                <InView as="div" onChange={(inView, entry) => inView && refetch()}>
                 {RecentlyAddedData && <ProductVerticalList title={'Recently added'} data={RecentlyAddedData}/>}
+                </InView>
             </SwiperSlide>
 
+
             <SwiperSlide>
+                <InView as="div" onChange={(inView, entry) => inView && refetch()}>
                 {topRatedData && <ProductVerticalList title={'Top Rated'} data={topRatedData}/>}
+                </InView>
             </SwiperSlide>
 
         </Swiper>
