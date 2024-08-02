79d03a7e4b32679d69ce19c8ec216164f03ddd3b
Fatemeh <fatemeh.saedchi@gmail.com>
2024-08-02 14:46:12 +0430

    implement accounts page

---
+++ src/mock/AllOrdersItem.ts
@@ -0,0 +1,39 @@
+export const allOrdersItem = [
+    {
+        id: 1357,
+        date: 'March 15, 2021',
+        status: 'Processing',
+        total: '$410.00 for 2 item',
+        actions: 'View'
+    },
+    {
+        id: 1358,
+        date: 'March 15, 2021',
+        status: 'Completed',
+        total: '$125.00 for 2 item',
+        actions: 'View'
+    },
+    {
+        id: 1359,
+        date: 'March 15, 2021',
+        status: 'Completed',
+        total: '$580.00 for 4 item',
+        actions: 'View'
+    },
+    {
+        id: 1360,
+        date: 'March 15, 2021',
+        status: 'Completed',
+        total: '$250.00 for 3 item',
+        actions: 'View'
+    }
+]
+
+
+
+
+// <div className="text-medium text-gray-500">#1357</div>
+// <div className="text-medium text-gray-500">March 15, 2021</div>
+// <div className="text-medium text-yellow-200">Processing</div>
+//     <div className="text-medium text-gray-500">$125.00 for 2 item</div>
+// <button className="text-medium text-green-200">View</button>
--- src/pages/accounts.tsx
+++ src/pages/accounts.tsx
@@ -1,5 +1,7 @@
 // @ts-ignore
 import {Section} from '@/components/layouts'
+import {YourCartTable} from "@/components";
+import {AllOrdersList} from "@/components/pages/accounts";
 interface Props {
     
 }
@@ -9,8 +11,7 @@ export default function Accounts({}: Props) {
         <>
             <Section className="lg:mt-[100px] sm:mt-4 font-lato mb-[239px]">
                 <div className="flex flex-wrap justify-center items-center">
-                    <div
-                        className="flex flex-col gap-2.5 font-quickSand text-gray-500 text-heading6 lg:mr-[73px] sm:mr-0 sm:mb-10">
+                    <div className="flex flex-col gap-2.5 font-quickSand text-gray-500 text-heading6 lg:mr-[73px] sm:mr-0 sm:mb-10">
                         <div
                             className="flex cursor-pointer gap-3.5 text-black px-4 py-3 rounded-[10px] items-center border border-gray-100 hover:bg-green-200 hover:text-white">
                             <img src="/assets/images/accounts/fi-rs-settings%201.svg" alt="image" width="26" height="24"/>
@@ -49,49 +50,8 @@ export default function Accounts({}: Props) {
                         </div>
                     </div>
 
-                    <div className="flex flex-col items-start">
-                        <div className="text-heading3 font-quickSand text-blue-300 mb-[22px]">Your Orders</div>
-                        <div
-                            className="flex flex-wrap bg-gray-100 justify-start items-start gap-32 rounded-2xl mb-[24px] py-[19px] px-[30px]">
-                            <div className="text-heading6 text-blue-300 font-quickSand gap">Order ID</div>
-                            <div className="text-heading6 text-blue-300 font-quickSand">Date</div>
-                            <div className="text-heading6 text-blue-300 font-quickSand">Status</div>
-                            <div className="text-heading6 text-blue-300 font-quickSand">Total</div>
-                            <div className="text-heading6 text-blue-300 font-quickSand">Actions</div>
-                        </div>
-                        <div
-                            className="flex flex-wrap justify-between items-start gap-20 mb-[30px] rounded-2xl px-[30px]">
-                            <div className="text-medium text-gray-500">#1357</div>
-                            <div className="text-medium text-gray-500">March 15, 2021</div>
-                            <div className="text-medium text-yellow-200">Processing</div>
-                            <div className="text-medium text-gray-500">$125.00 for 2 item</div>
-                            <button className="text-medium text-green-200">View</button>
-                        </div>
-                        <div
-                            className="flex flex-wrap justify-between items-start gap-20 rounded-2xl mb-[30px] px-[30px]">
-                            <div className="text-medium text-gray-500">#1357</div>
-                            <div className="text-medium text-gray-500">March 15, 2021</div>
-                            <div className="text-medium text-green-300">Completed</div>
-                            <div className="text-medium text-gray-500">$125.00 for 2 item</div>
-                            <button className="text-medium text-green-200">View</button>
-                        </div>
-                        <div
-                            className="flex flex-wrap justify-between items-start gap-20 rounded-2xl mb-[30px] px-[30px]">
-                            <div className="text-medium text-gray-500">#1357</div>
-                            <div className="text-medium text-gray-500">March 15, 2021</div>
-                            <div className="text-medium text-green-300">Completed</div>
-                            <div className="text-medium text-gray-500">$125.00 for 2 item</div>
-                            <button className="text-medium text-green-200">View</button>
-                        </div>
-                        <div
-                            className="flex flex-wrap justify-between items-start gap-20 rounded-2xl mb-[30px] px-[30px]">
-                            <div className="text-medium text-gray-500">#1357</div>
-                            <div className="text-medium text-gray-500">March 15, 2021</div>
-                            <div className="text-medium text-green-300">Completed</div>
-                            <div className="text-medium text-gray-500">$125.00 for 2 item</div>
-                            <button className="text-medium text-green-200">View</button>
-                        </div>
-                    </div>
+
+                    <AllOrdersList/>
                 </div>
             </Section>
         </>
